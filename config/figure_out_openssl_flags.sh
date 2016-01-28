7b800d5a35169fbf146d593ed017d3b01ad0822c
Jeremie Dimino <jdimino@janestreet.com>
2016-01-28 09:50:01 +0000

    Fix OSX build

---
+++ config/figure_out_openssl_flags.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+if [ -e setup.data ]; then
+    sed '/^openssl_\(lib\|cflags\)=/d' setup.data > setup.data.new
+    mv setup.data.new setup.data
+fi
+
+openssl_cclib="-lssl -lcrypto"
+openssl_ccopt=""
+
+if which pkg-config > /dev/null 2> /dev/null; then
+    # Hack for OSX
+    BREW_PREFIX="`brew --prefix 2> /dev/null || echo /`"
+    export PKG_CONFIG_PATH=$BREW_PREFIX/opt/openssl/lib/pkgconfig:$PKG_CONFIG_PATH
+    if pkg-config openssl 2> /dev/null; then
+        openssl_cclib="`pkg-config --libs openssl`"
+        openssl_ccopt="`pkg-config --cflags openssl`"
+    fi
+fi
+
+cat >> setup.data <<EOF
+openssl_cclib="$openssl_cclib"
+openssl_ccopt="$openssl_ccopt"
+EOF
+
--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 660d10b8514f290fec1c890d687b8f5d) *)
+(* DO NOT EDIT (digest: 8cf7ff08ecfa8431a24d5d92277409e7) *)
 module OASISGettext = struct
 (* # 22 "src/oasis/OASISGettext.ml" *)
 
@@ -613,14 +613,7 @@ let package_default =
      flags =
        [
           (["oasis_library_async_ssl_ccopt"; "compile"],
-            [(OASISExpr.EBool true, S [A "-ccopt"; A "-I${pkg_ctypes}/.."])]);
-          (["oasis_library_async_ssl_cclib"; "link"],
-            [
-               (OASISExpr.EBool true,
-                 S [A "-cclib"; A "-lssl"; A "-cclib"; A "-lcrypto"])
-            ]);
-          (["oasis_library_async_ssl_cclib"; "ocamlmklib"; "c"],
-            [(OASISExpr.EBool true, S [A "-lssl"; A "-lcrypto"])])
+            [(OASISExpr.EBool true, S [A "-ccopt"; A "-I${pkg_ctypes}/.."])])
        ];
      includes = [("stubgen", ["bindings"]); ("src", ["bindings"])]
   }
@@ -630,11 +623,42 @@ let conf = {MyOCamlbuildFindlib.no_automatic_syntax = false}
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default conf package_default;;
 
-# 634 "myocamlbuild.ml"
+# 627 "myocamlbuild.ml"
 (* OASIS_STOP *)
+
+let split str =
+  let rec skip_spaces i =
+    if i = String.length str then
+      []
+    else
+      if str.[i] = ' ' then
+        skip_spaces (i + 1)
+      else
+        extract i (i + 1)
+  and extract i j =
+    if j = String.length str then
+      [String.sub str i (j - i)]
+    else
+      if str.[j] = ' ' then
+        String.sub str i (j - i) :: skip_spaces (j + 1)
+      else
+        extract i (j + 1)
+  in
+  skip_spaces 0
+
+let get_flags var env =
+  try
+    List.map (fun x -> A x) (split (BaseEnvLight.var_get var env))
+  with Not_found ->
+    Printf.ksprintf failwith "Variable %s not defined in setup.data" var
 
 let dispatch = function
   | After_rules ->
+    let env = BaseEnvLight.load () in
+
+    let ccopt = get_flags "openssl_ccopt" env
+    and cclib = get_flags "openssl_cclib" env in
+
     let stubgen          = "stubgen/ffi_stubgen.byte" in
     let stubgen_types    = "stubgen/ffi_types_stubgen.byte" in
     let stubgen_ml_types = "stubgen/ffi_ml_types_subgen.exe" in
@@ -651,18 +675,18 @@ let dispatch = function
       (fun _ _ ->
          Cmd (S [P stubgen_types; Sh">"; A"stubgen/ffi_ml_types_stubgen.c"]));
 
-    rule "generated-types exe"
-      ~dep:"stubgen/ffi_ml_types_stubgen.c"
-      ~prod:stubgen_ml_types
-      (fun _ _ ->
-         let env = BaseEnvLight.load () in
-         let cc = BaseEnvLight.var_get "bytecomp_c_compiler" env in
-         let stdlib = BaseEnvLight.var_get "standard_library" env in
-         let ctypes = BaseEnvLight.var_get "pkg_ctypes" env in
-         Cmd (S [Sh cc; A"stubgen/ffi_ml_types_stubgen.c";
-                 A"-I"; P ctypes; A"-I"; P stdlib;
-                 A"-o"; A stubgen_ml_types])
-      );
+    (let cc = BaseEnvLight.var_get "bytecomp_c_compiler" env in
+     let stdlib : string = BaseEnvLight.var_get "standard_library" env in
+     let ctypes = BaseEnvLight.var_get "pkg_ctypes" env in
+     rule "generated-types exe"
+       ~dep:"stubgen/ffi_ml_types_stubgen.c"
+       ~prod:stubgen_ml_types
+       (fun _ _ ->
+          Cmd (S [Sh cc; A"stubgen/ffi_ml_types_stubgen.c";
+                  A"-I"; P ctypes; A"-I"; P stdlib;
+                  S ccopt;
+                  A"-o"; A stubgen_ml_types])
+       ));
 
     rule "generated-types ml"
       ~dep:stubgen_ml_types
@@ -676,7 +700,20 @@ let dispatch = function
       (fun _ _ ->
          Cmd(S[P stubgen; A"-c"; Sh">"; A"src/ffi_generated_stubs.c"]));
 
-    flag ["c"; "compile"] & S[A"-I"; A"src"; A"-package"; A"ctypes"]
+    flag ["c"; "compile"] & S[A"-I"; A"src"; A"-package"; A"ctypes"];
+
+    let tag_ccopt = "use_openssl_ccopt"
+    and tag_cclib = "use_openssl_cclib" in
+
+    (* Add flags for linking with the C library: *)
+    flag ["ocamlmklib"; "c"; tag_cclib] & S cclib;
+
+    (* C stubs using the C library must be compiled with the library
+       specifics flags: *)
+    flag ["c"; "compile"; tag_ccopt] & S (List.map (fun arg -> S[A"-ccopt"; arg]) ccopt);
+
+    (* OCaml libraries must depends on the C library: *)
+    flag ["link"; "ocaml"; tag_cclib] & S (List.map (fun arg -> S[A"-cclib"; arg]) cclib)
 
   | _ ->
     ()

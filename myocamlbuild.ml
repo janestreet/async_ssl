685a617fdc6f45c8c3eac412dcfba011ef28ec68
Jeremie Dimino <jdimino@janestreet.com>
2016-06-09 08:13:54 +0100

    added missing myocamlbuild.ml bits

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -59,8 +59,9 @@ let dispatch = function
       (fun _ _ ->
          Cmd (S [P stubgen_types; Sh">"; A"stubgen/ffi_ml_types_stubgen.c"]));
 
-    (let cc = BaseEnvLight.var_get "bytecomp_c_compiler" env in
-     let stdlib : string = BaseEnvLight.var_get "standard_library" env in
+    let cc = BaseEnvLight.var_get "bytecomp_c_compiler" env in
+
+    (let stdlib : string = BaseEnvLight.var_get "standard_library" env in
      let ctypes = BaseEnvLight.var_get "pkg_ctypes" env in
      rule "generated-types exe"
        ~dep:"stubgen/ffi_ml_types_stubgen.c"
@@ -71,6 +72,22 @@ let dispatch = function
                   S ccopt;
                   A"-o"; A stubgen_ml_types])
        ));
+
+    let discover_c   = "bindings/discover.c"   in
+    let discover_exe = "bindings/discover.exe" in
+    let config_h     = "bindings/config.h"     in
+
+    rule "discover tool"
+      ~dep:discover_c
+      ~prod:discover_exe
+      (fun _ _ ->
+         Cmd (S [Sh cc; A discover_c; S ccopt; S cclib; A"-ldl"; A"-o"; A discover_exe]));
+
+    rule "config.h file"
+      ~dep:discover_exe
+      ~prod:config_h
+      (fun _ _ ->
+         Cmd (S [P discover_exe; Sh">"; A config_h]));
 
     rule "generated-types ml"
       ~dep:stubgen_ml_types

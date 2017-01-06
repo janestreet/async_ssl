a47c2d3bc7739f4f130520ba6bdca19b84d853bf
Jeremie Dimino <jdimino@janestreet.com>
2017-01-06 17:17:38 +0000

    Replace the shell script by an OCaml program
    Fix #20

---
+++ bindings/config/discover.ml
@@ -0,0 +1,25 @@
+open Base
+open Stdio
+module C = Configurator
+
+let write_sexp fn sexp =
+  Out_channel.write_all fn ~data:(Sexp.to_string sexp)
+
+let () =
+  C.main ~name:"async_ssl" (fun c ->
+    let default : C.Pkg_config.package_conf =
+      { libs   = ["-lssl"; "-lcrypto"]
+      ; cflags = []
+      }
+    in
+    let conf =
+      match C.Pkg_config.get c with
+      | None -> default
+      | Some pc ->
+        Option.value (C.Pkg_config.query pc ~package:"openssl") ~default
+    in
+
+    write_sexp "openssl-cclib.sexp" [%sexp (conf.libs   : string list)];
+    write_sexp "openssl-ccopt.sexp" [%sexp (conf.cflags : string list)];
+    Out_channel.write_all "openssl-cclib" ~data:(String.concat conf.libs   ~sep:" ");
+    Out_channel.write_all "openssl-ccopt" ~data:(String.concat conf.cflags ~sep:" "))

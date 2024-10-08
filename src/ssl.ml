88ec2cdb372ce8b1c888adf6bad8304e95f901d3
public-release <as-public-release@janestreet.com>
2024-10-08 20:31:00 +0100

    v0.18~preview.130.00+55

--- src/ssl.ml
+++ src/ssl.ml
@@ -698,16 +698,15 @@ let server
   return (Ok conn)
 ;;
 
-let%test_module _ =
-  (module struct
-    let pipe_to_string reader = Pipe.to_list reader >>| String.concat
+module%test _ = struct
+  let pipe_to_string reader = Pipe.to_list reader >>| String.concat
 
-    let check_closed p name =
-      if not (Pipe.is_closed p) then failwith (name ^ " was left open.")
-    ;;
+  let check_closed p name =
+    if not (Pipe.is_closed p) then failwith (name ^ " was left open.")
+  ;;
 
-    (*
-       The pipe names are short because there's a lot of them and it got annoying to type.
+  (*
+     The pipe names are short because there's a lot of them and it got annoying to type.
        Please refer to this ascii art for an explanation.
 
        client_out
@@ -721,213 +720,204 @@ let%test_module _ =
        | SERVER |           | SSL 2 |            |
        +--------+ l <---- j +-------+ h <--------+
        server_in
-    *)
-    let with_pipes ~f =
-      let func = f in
-      if verbose then Debug.amf [%here] "creating pipes";
-      let l, j = Pipe.create () in
-      let h, e = Pipe.create () in
-      let c, a = Pipe.create () in
-      let b, d = Pipe.create () in
-      let f, g = Pipe.create () in
-      let i, k = Pipe.create () in
-      func ~a ~b ~c ~d ~e ~f ~g ~h ~i ~j ~k ~l
-    ;;
+  *)
+  let with_pipes ~f =
+    let func = f in
+    if verbose then Debug.amf [%here] "creating pipes";
+    let l, j = Pipe.create () in
+    let h, e = Pipe.create () in
+    let c, a = Pipe.create () in
+    let b, d = Pipe.create () in
+    let f, g = Pipe.create () in
+    let i, k = Pipe.create () in
+    func ~a ~b ~c ~d ~e ~f ~g ~h ~i ~j ~k ~l
+  ;;
 
-    (* Create both a client and a server, and send hello world back and forth. *)
-    let%test_unit _ =
-      let session = Session.create () in
-      let check_version conn =
-        (* Since Version.default is [Sslv23], we expect to negotiate the highest allowed
+  (* Create both a client and a server, and send hello world back and forth. *)
+  let%test_unit _ =
+    let session = Session.create () in
+    let check_version conn =
+      (* Since Version.default is [Sslv23], we expect to negotiate the highest allowed
            protocol version, which is [Tlsv1_3] *)
-        [%test_result: Version.t] (Connection.version conn) ~expect:Version.Tlsv1_3
+      [%test_result: Version.t] (Connection.version conn) ~expect:Version.Tlsv1_3
+    in
+    let check_session_reused conn ~expect =
+      [%test_result: bool] (Connection.session_reused conn) ~expect
+    in
+    let check_peer_certificate conn =
+      let cert =
+        Connection.peer_certificate conn |> Option.value_exn |> Or_error.ok_exn
       in
-      let check_session_reused conn ~expect =
-        [%test_result: bool] (Connection.session_reused conn) ~expect
+      let value =
+        let alist = Certificate.subject cert in
+        List.Assoc.find_exn alist ~equal:String.equal "CN"
       in
-      let check_peer_certificate conn =
-        let cert =
-          Connection.peer_certificate conn |> Option.value_exn |> Or_error.ok_exn
-        in
-        let value =
-          let alist = Certificate.subject cert in
-          List.Assoc.find_exn alist ~equal:String.equal "CN"
-        in
-        [%test_result: string] value ~expect:"testbox"
-      in
-      let run_test ~expect_session_reused =
-        with_pipes ~f:(fun ~a ~b ~c ~d ~e ~f ~g ~h ~i ~j ~k ~l ->
-          let client_in, client_out = b, a in
-          let server_in, server_out = l, k in
-          if verbose then Debug.amf [%here] "1";
-          (* attach the server to ssl 2 to net *)
-          let server_conn =
-            server
-              ~name:"server"
-                (* It might be confusing that the two "don't_use_in_production"
+      [%test_result: string] value ~expect:"testbox"
+    in
+    let run_test ~expect_session_reused =
+      with_pipes ~f:(fun ~a ~b ~c ~d ~e ~f ~g ~h ~i ~j ~k ~l ->
+        let client_in, client_out = b, a in
+        let server_in, server_out = l, k in
+        if verbose then Debug.amf [%here] "1";
+        (* attach the server to ssl 2 to net *)
+        let server_conn =
+          server
+            ~name:"server"
+              (* It might be confusing that the two "don't_use_in_production"
                  files are used for different purposes. This is enough to test out
                  the functionality, but if we want to be super clear we need 5
                  such files in this library: ca crt, server key + crt, and client
                  key + crt.*)
-              ~allowed_ciphers:`Secure
-              ~ca_file:"do_not_use_in_production.crt" (* CA certificate *)
-              ~crt_file:"do_not_use_in_production.crt" (* server certificate *)
-              ~key_file:"do_not_use_in_production.key" (* server key *)
-              ~verify_modes:[ Verify_mode.Verify_peer ]
-              ~app_to_ssl:i
-              ~ssl_to_app:j
-              ~ssl_to_net:g
-              ~net_to_ssl:h
-              ()
-          in
-          let client_conn =
-            client
-              ~name:"client" (* Necessary to verify the self-signed server certificate. *)
-              ~allowed_ciphers:`Secure
-              ~ca_file:"do_not_use_in_production.crt" (* ca certificate *)
-              ~crt_file:"do_not_use_in_production.crt" (* client certificate *)
-              ~key_file:"do_not_use_in_production.key" (* client key *)
-              ~hostname:"does-not-matter"
-              ~session
-              ~app_to_ssl:c
-              ~ssl_to_app:d
-              ~ssl_to_net:e
-              ~net_to_ssl:f
-              ()
-          in
-          let client_conn = client_conn >>| Or_error.ok_exn in
-          let server_conn = server_conn >>| Or_error.ok_exn in
-          let%bind client_conn, server_conn = Deferred.both client_conn server_conn in
-          check_version client_conn;
-          check_version server_conn;
-          if verbose then Debug.amf [%here] "client checking server certificate";
-          check_peer_certificate client_conn;
-          if verbose then Debug.amf [%here] "server checking client certificate";
-          check_peer_certificate server_conn;
-          if verbose then Debug.amf [%here] "client checking reused";
-          check_session_reused client_conn ~expect:expect_session_reused;
-          if verbose then Debug.amf [%here] "server checking reused";
-          check_session_reused server_conn ~expect:expect_session_reused;
-          if verbose then Debug.amf [%here] "2";
-          Pipe.write client_out "hello, server." |> don't_wait_for;
-          if verbose then Debug.amf [%here] "3";
-          Pipe.close client_out;
-          if verbose then Debug.amf [%here] "4";
-          Pipe.write server_out "hello, client." |> don't_wait_for;
-          if verbose then Debug.amf [%here] "5";
-          Pipe.close server_out;
-          if verbose then Debug.amf [%here] "6";
-          let%bind on_server = pipe_to_string server_in in
-          if verbose then Debug.amf [%here] "7";
-          let%bind on_client = pipe_to_string client_in in
-          if verbose then Debug.amf [%here] "8";
-          (* check that all the pipes are closed *)
-          check_closed a "client_in";
-          check_closed b "client_out";
-          check_closed c "c";
-          check_closed d "d";
-          check_closed e "e";
-          check_closed f "f";
-          check_closed g "g";
-          check_closed h "h";
-          check_closed i "i";
-          check_closed j "j";
-          check_closed k "server_in";
-          check_closed l "server_out";
-          if verbose then Debug.amf [%here] "9";
-          let%bind client_exit_status = Connection.closed client_conn in
-          Or_error.ok_exn client_exit_status;
-          let%bind server_exit_status = Connection.closed server_conn in
-          Or_error.ok_exn server_exit_status;
-          if on_server <> "hello, server."
-          then
-            failwiths
-              ~here:[%here]
-              "No hello world to server"
-              on_server
-              [%sexp_of: string];
-          if on_client <> "hello, client."
-          then
-            failwiths
-              ~here:[%here]
-              "No hello world to client"
-              on_client
-              [%sexp_of: string];
-          return ())
-      in
-      let run_twice () =
-        if verbose then Debug.amf [%here] "first run";
-        let%bind () = run_test ~expect_session_reused:false in
-        if verbose then Debug.amf [%here] "second run";
-        run_test ~expect_session_reused:false
-      in
-      Thread_safe.block_on_async_exn run_twice
-    ;;
+            ~allowed_ciphers:`Secure
+            ~ca_file:"do_not_use_in_production.crt" (* CA certificate *)
+            ~crt_file:"do_not_use_in_production.crt" (* server certificate *)
+            ~key_file:"do_not_use_in_production.key" (* server key *)
+            ~verify_modes:[ Verify_mode.Verify_peer ]
+            ~app_to_ssl:i
+            ~ssl_to_app:j
+            ~ssl_to_net:g
+            ~net_to_ssl:h
+            ()
+        in
+        let client_conn =
+          client
+            ~name:"client" (* Necessary to verify the self-signed server certificate. *)
+            ~allowed_ciphers:`Secure
+            ~ca_file:"do_not_use_in_production.crt" (* ca certificate *)
+            ~crt_file:"do_not_use_in_production.crt" (* client certificate *)
+            ~key_file:"do_not_use_in_production.key" (* client key *)
+            ~hostname:"does-not-matter"
+            ~session
+            ~app_to_ssl:c
+            ~ssl_to_app:d
+            ~ssl_to_net:e
+            ~net_to_ssl:f
+            ()
+        in
+        let client_conn = client_conn >>| Or_error.ok_exn in
+        let server_conn = server_conn >>| Or_error.ok_exn in
+        let%bind client_conn, server_conn = Deferred.both client_conn server_conn in
+        check_version client_conn;
+        check_version server_conn;
+        if verbose then Debug.amf [%here] "client checking server certificate";
+        check_peer_certificate client_conn;
+        if verbose then Debug.amf [%here] "server checking client certificate";
+        check_peer_certificate server_conn;
+        if verbose then Debug.amf [%here] "client checking reused";
+        check_session_reused client_conn ~expect:expect_session_reused;
+        if verbose then Debug.amf [%here] "server checking reused";
+        check_session_reused server_conn ~expect:expect_session_reused;
+        if verbose then Debug.amf [%here] "2";
+        Pipe.write client_out "hello, server." |> don't_wait_for;
+        if verbose then Debug.amf [%here] "3";
+        Pipe.close client_out;
+        if verbose then Debug.amf [%here] "4";
+        Pipe.write server_out "hello, client." |> don't_wait_for;
+        if verbose then Debug.amf [%here] "5";
+        Pipe.close server_out;
+        if verbose then Debug.amf [%here] "6";
+        let%bind on_server = pipe_to_string server_in in
+        if verbose then Debug.amf [%here] "7";
+        let%bind on_client = pipe_to_string client_in in
+        if verbose then Debug.amf [%here] "8";
+        (* check that all the pipes are closed *)
+        check_closed a "client_in";
+        check_closed b "client_out";
+        check_closed c "c";
+        check_closed d "d";
+        check_closed e "e";
+        check_closed f "f";
+        check_closed g "g";
+        check_closed h "h";
+        check_closed i "i";
+        check_closed j "j";
+        check_closed k "server_in";
+        check_closed l "server_out";
+        if verbose then Debug.amf [%here] "9";
+        let%bind client_exit_status = Connection.closed client_conn in
+        Or_error.ok_exn client_exit_status;
+        let%bind server_exit_status = Connection.closed server_conn in
+        Or_error.ok_exn server_exit_status;
+        if on_server <> "hello, server."
+        then
+          failwiths ~here:[%here] "No hello world to server" on_server [%sexp_of: string];
+        if on_client <> "hello, client."
+        then
+          failwiths ~here:[%here] "No hello world to client" on_client [%sexp_of: string];
+        return ())
+    in
+    let run_twice () =
+      if verbose then Debug.amf [%here] "first run";
+      let%bind () = run_test ~expect_session_reused:false in
+      if verbose then Debug.amf [%here] "second run";
+      run_test ~expect_session_reused:false
+    in
+    Thread_safe.block_on_async_exn run_twice
+  ;;
 
-    let%bench "ssl_stress_test" =
-      let run_bench () =
-        with_pipes ~f:(fun ~a ~b ~c ~d ~e ~f ~g ~h ~i ~j ~k ~l ->
-          let client_in, client_out = b, a in
-          let server_in, server_out = l, k in
-          (* attach the server to ssl 2 to net *)
-          let server_conn =
-            server
-              ~name:"server"
-              ~allowed_ciphers:`Secure
-              ~crt_file:"do_not_use_in_production.crt"
-              ~key_file:"do_not_use_in_production.key"
-              ~app_to_ssl:i
-              ~ssl_to_app:j
-              ~ssl_to_net:g
-              ~net_to_ssl:h
-              ()
-          in
-          (* attach the client to ssl 1 to net *)
-          let client_conn =
-            client
-              ~name:"client"
-              ~allowed_ciphers:`Secure
-              ~app_to_ssl:c
-              ~ssl_to_app:d
-              ~ssl_to_net:e
-              ~net_to_ssl:f
-              ()
-          in
-          let%bind client_conn, server_conn = Deferred.both client_conn server_conn in
-          let client_conn = Or_error.ok_exn client_conn in
-          let server_conn = Or_error.ok_exn server_conn in
-          let rec cycle k =
-            if k = 0
-            then (
-              Pipe.close client_out;
-              Pipe.close server_out;
-              return ())
-            else (
-              let%bind () = Pipe.write client_out "hello server" in
-              match%bind Pipe.read server_in with
-              | `Eof -> assert false
-              | `Ok s ->
-                let%bind () =
-                  assert (s = "hello server");
-                  Pipe.write server_out "hello client"
-                in
-                (match%bind Pipe.read client_in with
-                 | `Eof -> assert false
-                 | `Ok s ->
-                   let%bind () =
-                     assert (s = "hello client");
-                     return ()
-                   in
-                   cycle (k - 1)))
-          in
-          let%bind () = cycle 1_000 in
-          let%bind client_exit_status = Connection.closed client_conn in
-          Or_error.ok_exn client_exit_status;
-          let%bind server_exit_status = Connection.closed server_conn in
-          Or_error.ok_exn server_exit_status;
-          return ())
-      in
-      Thread_safe.block_on_async_exn run_bench
-    ;;
-  end)
-;;
+  let%bench "ssl_stress_test" =
+    let run_bench () =
+      with_pipes ~f:(fun ~a ~b ~c ~d ~e ~f ~g ~h ~i ~j ~k ~l ->
+        let client_in, client_out = b, a in
+        let server_in, server_out = l, k in
+        (* attach the server to ssl 2 to net *)
+        let server_conn =
+          server
+            ~name:"server"
+            ~allowed_ciphers:`Secure
+            ~crt_file:"do_not_use_in_production.crt"
+            ~key_file:"do_not_use_in_production.key"
+            ~app_to_ssl:i
+            ~ssl_to_app:j
+            ~ssl_to_net:g
+            ~net_to_ssl:h
+            ()
+        in
+        (* attach the client to ssl 1 to net *)
+        let client_conn =
+          client
+            ~name:"client"
+            ~allowed_ciphers:`Secure
+            ~app_to_ssl:c
+            ~ssl_to_app:d
+            ~ssl_to_net:e
+            ~net_to_ssl:f
+            ()
+        in
+        let%bind client_conn, server_conn = Deferred.both client_conn server_conn in
+        let client_conn = Or_error.ok_exn client_conn in
+        let server_conn = Or_error.ok_exn server_conn in
+        let rec cycle k =
+          if k = 0
+          then (
+            Pipe.close client_out;
+            Pipe.close server_out;
+            return ())
+          else (
+            let%bind () = Pipe.write client_out "hello server" in
+            match%bind Pipe.read server_in with
+            | `Eof -> assert false
+            | `Ok s ->
+              let%bind () =
+                assert (s = "hello server");
+                Pipe.write server_out "hello client"
+              in
+              (match%bind Pipe.read client_in with
+               | `Eof -> assert false
+               | `Ok s ->
+                 let%bind () =
+                   assert (s = "hello client");
+                   return ()
+                 in
+                 cycle (k - 1)))
+        in
+        let%bind () = cycle 1_000 in
+        let%bind client_exit_status = Connection.closed client_conn in
+        Or_error.ok_exn client_exit_status;
+        let%bind server_exit_status = Connection.closed server_conn in
+        Or_error.ok_exn server_exit_status;
+        return ())
+    in
+    Thread_safe.block_on_async_exn run_bench
+  ;;
+end
